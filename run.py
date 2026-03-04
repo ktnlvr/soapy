91c71f7188ad346e000664937c3896e6eedc7c93
Artur Roos <artur.roos@ktnlvr.dev>
2026-03-04 21:39:36 +0200

    Initial commit

---
+++ run.py
@@ -0,0 +1,129 @@
+from numba import jit
+import numba as nb
+import math
+import numpy as np
+
+from ase.io import read
+
+from scipy.special import gamma
+from scipy.linalg import sqrtm, inv
+
+
+def get_basis_gto(r_cut, n_max, l_max):
+    a = np.linspace(1, r_cut, n_max)
+    threshold = 1e-3
+    alphas_full = np.zeros((l_max + 1, n_max))
+    betas_full = np.zeros((l_max + 1, n_max, n_max))
+
+    for l in range(l_max + 1):
+        alphas = -np.log(threshold / np.power(a, l)) / a**2
+        m = np.zeros((alphas.shape[0], alphas.shape[0])) + alphas
+        m = m + m.T
+        S = 0.5 * gamma(l + 3.0 / 2.0) * m ** (-l - 3.0 / 2.0)
+        betas = sqrtm(inv(S))
+        if betas.dtype == np.complex128:
+            raise ValueError(
+                "Could not calculate normalization factors for the radial "
+                "basis in the domain of real numbers. Lower n_max or increase r_cut."
+            )
+        alphas_full[l, :] = alphas
+        betas_full[l, :, :] = betas
+    return alphas_full, betas_full
+
+
+def precompute_xi_lmk(l_max):
+    xi = np.zeros((l_max + 1, l_max + 1, l_max + 1))
+
+    for l in range(l_max + 1):
+        for m in range(l + 1):
+            for k in range(m, l + 1):
+                if (k - l) % 2 != 0:
+                    xi[l, m, k] = 0.0
+                else:
+                    num = math.gamma((l + k - 1)/2 + 1)
+                    den = (
+                        math.gamma(k - m + 1) *
+                        math.gamma(l - k + 1) *
+                        math.gamma((l + k - 1)/2 - l + 1)
+                    )
+                    xi[l, m, k] = num / den
+    return xi
+
+
+
+def compute_c_nlm(
+    l, m,
+    K_nlm,
+    E_nlm,
+    R2,
+    xy_pow,
+    R_pow,
+    z_pow,
+    xi_lmk
+):
+    pass
+
+def mathcalK(p, x, y, z, n, l, m, E, R_pow, xi_lmk):
+    ...
+
+def precompute_K_nlm(alpha_bl, beta_nbl, sigma):
+    n_max = beta_nbl.shape[1]
+    l_max = alpha_bl.shape[0] - 1
+
+    K_nlm = np.zeros((n_max, l_max+1, l_max+1))
+
+    for l in range(l_max + 1):
+        for m in range(l + 1):
+            #            ----------------------|
+            #          l | (2l + 1) * (l - m)! |
+            # 𝜆_lm = 2   | ~~~~~~~~~~~~~~~~~~~ |
+            #            v    4𝜋(l + m!)       | 
+            #
+
+            numerator = (2*l + 1) * math.factorial(l - m)
+            denominator = 4*math.pi * math.factorial(l + m)
+            lambda_lm = (2**l) * math.sqrt(numerator / denominator)
+
+            for n in range(n_max):
+                #                            b_lbn
+                # \Sigma_N_b = ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                #               \sqrt { 1 + 2 a_lb 𝜎^2 }^(2l+3)
+                sum_b = 0.0
+                for b in range(n_max):
+                    ab = alpha_bl[l, b]
+                    bb = beta_nbl[l, n, b]
+                    denom = (1 + 2*ab*sigma*sigma)**(l + 3/2)
+                    sum_b += bb / denom
+
+                # \sqrt{ 2 𝜋 𝜎^2 }^3
+                K_nlm[n, l, m] = lambda_lm * (2*math.pi*sigma*sigma)**(3/2) * sum_b
+
+    return K_nlm
+
+def calcK():
+    ...
+
+def main():
+    r_cut = 50
+    n_max = 2
+    l_max = 3
+    sigma = 1
+
+    xi_lmk_table = precompute_xi_lmk(l_max)
+    assert not np.any(np.isnan(xi_lmk_table))
+    print(f"Xi_lmk = {xi_lmk_table.shape}")
+
+    alpha_bl, beta_nbl = get_basis_gto(r_cut, n_max, l_max)
+    print(f"A_bl = {alpha_bl.shape}")
+    print(f"B_nbl = {beta_nbl.shape}")
+
+    atoms = read('random_hydrogens.xyz')
+    positions = atoms.positions
+    x_p, y_p, z_p = positions[:,0], positions[:,1], positions[:,2]
+
+    print(f"N_p = {len(positions)}")
+
+    k_nlm = precompute_K_nlm(alpha_bl, beta_nbl, sigma)
+    print(f"k_nlm = {k_nlm.shape}")
+
+main()
