df5be8b70cf84ba8deae28630302a0c38a77b8ed
Artur Roos <artur.roos@ktnlvr.dev>
2026-03-05 18:37:36 +0200

    Generate precompute buffers

--- run.py
+++ run.py
@@ -8,6 +8,18 @@ from ase.io import read
 from scipy.special import gamma
 from scipy.linalg import sqrtm, inv
 
+import time
+
+
+def tic():
+    global start_time
+    start_time = time.time()
+
+def toc():
+    global start_time, elapsed_time
+    elapsed_time += (time.time() - start_time) * 1_000_000
+    print(elapsed_time)
+
 
 def get_basis_gto(r_cut, n_max, l_max):
     a = np.linspace(1, r_cut, n_max)
@@ -49,9 +61,13 @@ def precompute_xi_lmk(l_max):
                     xi[l, m, k] = num / den
     return xi
 
+# C_nlm = K_lm 
+#   \Sigma_b W_nlb 
+#   \Sigma_p exp( E_lb * R^2 )
+#      (x_p + iy_p)^m \Sigma_{k=m}^l 
+#         Xi_lmk z^{k-m}_p R^{l-k}_p
 
-
-def compute_c_nlm(
+def compute_c_nlm(n, 
     l, m,
     K_nlm,
     E_nlm,
@@ -61,13 +77,17 @@ def compute_c_nlm(
     z_pow,
     xi_lmk
 ):
-    pass
+    ...
 
 def mathcalK(p, x, y, z, n, l, m, E, R_pow, xi_lmk):
     ...
 
-def precompute_K_nlm(alpha_bl, beta_nbl, sigma):
-    n_max = beta_nbl.shape[1]
+def precompute_W_nlb(alpha_bl, beta_lnb, sigma):
+    denom = (1.0 + 2.0 * alpha_bl * sigma * sigma) ** 1.5
+    return (beta_lnb / denom[:, None, :]).transpose(1, 0, 2)
+
+def precompute_K_nlm(alpha_bl, beta_lnb, sigma):
+    n_max = beta_lnb.shape[1]
     l_max = alpha_bl.shape[0] - 1
 
     K_nlm = np.zeros((n_max, l_max+1, l_max+1))
@@ -75,7 +95,7 @@ def precompute_K_nlm(alpha_bl, beta_nbl, sigma):
     for l in range(l_max + 1):
         for m in range(l + 1):
             #            ----------------------|
-            #          l | (2l + 1) * (l - m)! |
+            #         l  | (2l + 1) * (l - m)! |
             # 𝜆_lm = 2   | ~~~~~~~~~~~~~~~~~~~ |
             #            v    4𝜋(l + m!)       | 
             #
@@ -85,13 +105,13 @@ def precompute_K_nlm(alpha_bl, beta_nbl, sigma):
             lambda_lm = (2**l) * math.sqrt(numerator / denominator)
 
             for n in range(n_max):
-                #                            b_lbn
+                #                            B_lbn
                 # \Sigma_N_b = ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 #               \sqrt { 1 + 2 a_lb 𝜎^2 }^(2l+3)
                 sum_b = 0.0
                 for b in range(n_max):
                     ab = alpha_bl[l, b]
-                    bb = beta_nbl[l, n, b]
+                    bb = beta_lnb[l, n, b]
                     denom = (1 + 2*ab*sigma*sigma)**(l + 3/2)
                     sum_b += bb / denom
 
@@ -100,8 +120,11 @@ def precompute_K_nlm(alpha_bl, beta_nbl, sigma):
 
     return K_nlm
 
-def calcK():
-    ...
+
+def precompute_E_lb(alpha_bl, sigma):
+    denom = 1.0 + 2.0 * alpha_bl * sigma * sigma
+    E_lb = -alpha_bl / denom
+    return E_lb
 
 def main():
     r_cut = 50
@@ -113,9 +136,9 @@ def main():
     assert not np.any(np.isnan(xi_lmk_table))
     print(f"Xi_lmk = {xi_lmk_table.shape}")
 
-    alpha_bl, beta_nbl = get_basis_gto(r_cut, n_max, l_max)
+    alpha_bl, beta_lnb = get_basis_gto(r_cut, n_max, l_max)
     print(f"A_bl = {alpha_bl.shape}")
-    print(f"B_nbl = {beta_nbl.shape}")
+    print(f"B_lnb = {beta_lnb.shape}")
 
     atoms = read('random_hydrogens.xyz')
     positions = atoms.positions
@@ -123,7 +146,27 @@ def main():
 
     print(f"N_p = {len(positions)}")
 
-    k_nlm = precompute_K_nlm(alpha_bl, beta_nbl, sigma)
-    print(f"k_nlm = {k_nlm.shape}")
+    k_nlm = precompute_K_nlm(alpha_bl, beta_lnb, sigma)
+    print(f"K_nlm = {k_nlm.shape}")
+
+    w_nlb = precompute_W_nlb(alpha_bl, beta_lnb, sigma)
+    print(f"W_nlb = {w_nlb.shape}")
+
+    e_lb = precompute_E_lb(alpha_bl, sigma)
+    print(f"E_lb = {e_lb.shape}")
+
+    c_arr = np.zeros((n_max, l_max+1, l_max+1), dtype=complex)
+    c = []
+
+    tic()
+    for nn in range(n_max):
+        c.append([])
+        for ln in range(l_max+1):
+            c[nn].append([])
+            for mn in range(ln + 1):
+                c_val = compute_c_nlm(nn, ln, mn, k_nlm, x_p, y_p, z_p, sigma)
+                c_arr[nn, ln, mn] = c_val
+                c[nn][ln].append(c_val)
+    toc()
 
 main()
