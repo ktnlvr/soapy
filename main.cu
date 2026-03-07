a02ec02a6f04a010635f2941cc44508ed5098edd
Artur Roos <artur.roos@ktnlvr.dev>
2026-03-07 16:23:32 +0200

    Asyncronously initialise the cuda kernel

--- main.cu
+++ main.cu
@@ -1,14 +1,15 @@
-#include <cuda_runtime.h>
 #include <cuda_profiler_api.h>
+#include <cuda_runtime.h>
 
 #include <gsl/gsl_eigen.h>
 #include <gsl/gsl_matrix.h>
 #include <math.h>
+#include <pthread.h>
 #include <stdlib.h>
 
+#include "include/kernels.h"
 #include "include/matrix.h"
 #include "include/xyz.h"
-#include "include/kernels.h"
 
 static inline int index_xi_lmk(int l, int m, int k, int l_max) {
   return l * (l_max + 1) * (l_max + 1) + m * (l_max + 1) + k;
@@ -184,142 +185,162 @@ double *precompute_E_lb(const double *alpha_bl, int l_max, int n_max,
 
   return E_lb;
 }
-#include <cuda_runtime.h>
-#include <stdio.h>
 
-// assume compute_c_nlm_kernel is defined somewhere
+static cudaEvent_t start, stop;
+static  cudaStream_t compute_stream;
+
+void *cuda_init_thread(void *arg) {
+  cudaError_t err = cudaFree(0);
+  if (err != cudaSuccess) {
+    fprintf(stderr, "CUDA initialization failed: %s\n",
+            cudaGetErrorString(err));
+  }
+  
+  cudaDeviceSynchronize();
+  cudaEventCreate(&start);
+  cudaStreamCreate(&compute_stream);
+  cudaEventRecord(start, compute_stream);
+
+  return NULL;
+}
 
 int main(void) {
-    // Initialise lazy CUDA context
-    cudaFree(0);
+  pthread_t init_thread;
+  pthread_create(&init_thread, NULL, cuda_init_thread, NULL);
 
-    // ----------------------
-    // Create two streams
-    // ----------------------
-    cudaStream_t compute_stream;
-    cudaStream_t transfer_stream;
-    cudaStreamCreate(&compute_stream);
-    cudaStreamCreate(&transfer_stream);
+  int r_cut = 50;
+  int n_max = 2;
+  int l_max = 3;
+  int sigma = 1;
 
-    int r_cut = 50;
-    int n_max = 2;
-    int l_max = 3;
-    int sigma = 1;
+  int size = (l_max + 1) * (l_max + 1) * (l_max + 1);
+  double *xi_lmk_cpu = (double *)malloc(size * sizeof(double));
+  precompute_xi_lmk(xi_lmk_cpu, l_max);
 
-    int size = (l_max + 1) * (l_max + 1) * (l_max + 1);
-    double *xi_lmk_cpu = (double *)malloc(size * sizeof(double));
-    precompute_xi_lmk(xi_lmk_cpu, l_max);
+  double *alpha_bl;
+  double *beta_lnb;
+  get_basis_gto(5.0, 6, 4, &alpha_bl, &beta_lnb);
 
-    double *alpha_bl;
-    double *beta_lnb;
-    get_basis_gto(5.0, 6, 4, &alpha_bl, &beta_lnb);
+  int N_p;
+  double *x_out, *y_out, *z_out;
+  int err =
+      read_xyz_coords("random_hydrogens.xyz", &x_out, &y_out, &z_out, &N_p);
+  if (err) {
+    fprintf(stderr, "Error reading XYZ file: %d\n", err);
+    return 1;
+  }
 
-    int N_p;
-    double *x_out, *y_out, *z_out;
-    int err = read_xyz_coords("random_hydrogens.xyz", &x_out, &y_out, &z_out, &N_p);
-    if (err) { fprintf(stderr,"Error reading XYZ file: %d\n", err); return 1; }
+  double *K = precompute_K_nlm(alpha_bl, beta_lnb, n_max, l_max, sigma);
+  double *E = precompute_E_lb(alpha_bl, l_max, n_max, sigma);
+  double *W = precompute_W_nlb(alpha_bl, beta_lnb, l_max, n_max, sigma);
 
-    double *K = precompute_K_nlm(alpha_bl, beta_lnb, n_max, l_max, sigma);
-    double *E = precompute_E_lb(alpha_bl, l_max, n_max, sigma);
-    double *W = precompute_W_nlb(alpha_bl, beta_lnb, l_max, n_max, sigma);
+  int L = l_max + 1;
+  size_t size_alpha = L * n_max;
+  size_t size_beta = L * n_max * n_max;
+  size_t size_K = n_max * L * L;
+  size_t size_E = L * n_max;
+  size_t size_W = n_max * L * n_max;
+  int size_xi = L * L * L;
+  size_t total_size =
+      (size_alpha + size_beta + size_K + size_E + size_W + size_xi) *
+      sizeof(double);
 
-    int L = l_max + 1;
-    size_t size_alpha = L * n_max;        
-    size_t size_beta  = L * n_max * n_max; 
-    size_t size_K     = n_max * L * L;    
-    size_t size_E     = L * n_max;        
-    size_t size_W     = n_max * L * n_max;
-    int size_xi       = L * L * L;
-    size_t total_size = (size_alpha + size_beta + size_K + size_E + size_W + size_xi) * sizeof(double);
+  pthread_join(init_thread, NULL);
 
-    double *d_mem;
-    cudaMalloc(&d_mem, total_size);
-    double *d_alpha = d_mem;
-    double *d_beta  = d_alpha + size_alpha;
-    double *d_K     = d_beta + size_beta;
-    double *d_E     = d_K + size_K;
-    double *d_W     = d_E + size_E;
-    double *d_xi    = d_W + size_W;
+  cudaStream_t transfer_stream;
+  cudaStreamCreate(&transfer_stream);
 
-    double *xyz_dev;
-    cudaMalloc(&xyz_dev, 3 * N_p * sizeof(double));
-    double *d_x_out = xyz_dev;
-    double *d_y_out = xyz_dev + N_p;
-    double *d_z_out = xyz_dev + 2 * N_p;
+  double *d_mem;
+  cudaMalloc(&d_mem, total_size);
+  double *d_alpha = d_mem;
+  double *d_beta = d_alpha + size_alpha;
+  double *d_K = d_beta + size_beta;
+  double *d_E = d_K + size_K;
+  double *d_W = d_E + size_E;
+  double *d_xi = d_W + size_W;
 
-    cudaMemcpyAsync(d_alpha, alpha_bl, size_alpha*sizeof(double), cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_beta,  beta_lnb, size_beta*sizeof(double),  cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_K,     K,        size_K*sizeof(double),     cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_E,     E,        size_E*sizeof(double),     cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_W,     W,        size_W*sizeof(double),     cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_xi,    xi_lmk_cpu, size_xi*sizeof(double),  cudaMemcpyHostToDevice, transfer_stream);
+  double *xyz_dev;
+  cudaMalloc(&xyz_dev, 3 * N_p * sizeof(double));
+  double *d_x_out = xyz_dev;
+  double *d_y_out = xyz_dev + N_p;
+  double *d_z_out = xyz_dev + 2 * N_p;
 
-    cudaMemcpyAsync(d_x_out, x_out, N_p * sizeof(double), cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_y_out, y_out, N_p * sizeof(double), cudaMemcpyHostToDevice, transfer_stream);
-    cudaMemcpyAsync(d_z_out, z_out, N_p * sizeof(double), cudaMemcpyHostToDevice, transfer_stream);
+  cudaMemcpyAsync(d_alpha, alpha_bl, size_alpha * sizeof(double),
+                  cudaMemcpyHostToDevice, transfer_stream);
+  cudaMemcpyAsync(d_beta, beta_lnb, size_beta * sizeof(double),
+                  cudaMemcpyHostToDevice, transfer_stream);
+  cudaMemcpyAsync(d_K, K, size_K * sizeof(double), cudaMemcpyHostToDevice,
+                  transfer_stream);
+  cudaMemcpyAsync(d_E, E, size_E * sizeof(double), cudaMemcpyHostToDevice,
+                  transfer_stream);
+  cudaMemcpyAsync(d_W, W, size_W * sizeof(double), cudaMemcpyHostToDevice,
+                  transfer_stream);
+  cudaMemcpyAsync(d_xi, xi_lmk_cpu, size_xi * sizeof(double),
+                  cudaMemcpyHostToDevice, transfer_stream);
 
-    size_t size_c = N_p * n_max * L * L * sizeof(double);
-    double *d_c_real, *d_c_imag;
-    cudaMalloc(&d_c_real, size_c);
-    cudaMalloc(&d_c_imag, size_c);
+  cudaMemcpyAsync(d_x_out, x_out, N_p * sizeof(double), cudaMemcpyHostToDevice,
+                  transfer_stream);
+  cudaMemcpyAsync(d_y_out, y_out, N_p * sizeof(double), cudaMemcpyHostToDevice,
+                  transfer_stream);
+  cudaMemcpyAsync(d_z_out, z_out, N_p * sizeof(double), cudaMemcpyHostToDevice,
+                  transfer_stream);
 
-    double *c_real_host = (double *)malloc(size_c);
-    double *c_imag_host = (double *)malloc(size_c);
+  size_t size_c = N_p * n_max * L * L * sizeof(double);
+  double *d_c_real, *d_c_imag;
+  cudaMalloc(&d_c_real, size_c);
+  cudaMalloc(&d_c_imag, size_c);
 
-    cudaStreamSynchronize(transfer_stream);
+  double *c_real_host = (double *)malloc(size_c);
+  double *c_imag_host = (double *)malloc(size_c);
 
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    
-    cudaEventRecord(start, compute_stream);
+  cudaStreamSynchronize(transfer_stream);
 
-    int threads = 256;
-    int blocks = (N_p + threads - 1)/threads;
+  cudaEventCreate(&stop);
 
-    compute_c_nlm_kernel<<<blocks, threads, 0, compute_stream>>>(
-        N_p, n_max, l_max,
-        d_W, d_E, d_xi,
-        d_x_out, d_y_out, d_z_out,
-        d_c_real, d_c_imag
-    );
 
-    cudaEventRecord(stop, compute_stream);
+  int threads = 256;
+  int blocks = (N_p + threads - 1) / threads;
 
-    cudaStreamSynchronize(compute_stream);
+  compute_c_nlm_kernel<<<blocks, threads, 0, compute_stream>>>(
+      N_p, n_max, l_max, d_W, d_E, d_xi, d_x_out, d_y_out, d_z_out, d_c_real,
+      d_c_imag);
 
-    cudaEventSynchronize(stop);
+  cudaEventRecord(stop, compute_stream);
 
-    float milliseconds = 0;
-    cudaEventElapsedTime(&milliseconds, start, stop);
+  cudaStreamSynchronize(compute_stream);
 
-    double elapsed_us = milliseconds * 1000.0;
-    double elapsed_s  = milliseconds / 1000.0;
+  cudaEventSynchronize(stop);
 
-    printf("Elapsed time: %.6f μs (%.6f s)\n", elapsed_us, elapsed_s);
+  float milliseconds = 0;
+  cudaEventElapsedTime(&milliseconds, start, stop);
 
-    cudaStreamSynchronize(compute_stream);
+  double elapsed_us = milliseconds * 1000.0;
+  double elapsed_s = milliseconds / 1000.0;
 
-    cudaFree(d_mem);
-    cudaFree(xyz_dev);
-    cudaFree(d_c_real);
-    cudaFree(d_c_imag);
+  printf("Elapsed time: %.6f μs (%.6f s)\n", elapsed_us, elapsed_s);
 
-    cudaStreamDestroy(compute_stream);
-    cudaStreamDestroy(transfer_stream);
+  cudaStreamSynchronize(compute_stream);
 
-    cudaEventDestroy(start);
-    cudaEventDestroy(stop);
+  cudaFree(d_mem);
+  cudaFree(xyz_dev);
+  cudaFree(d_c_real);
+  cudaFree(d_c_imag);
 
-    free(xi_lmk_cpu);
-    free(alpha_bl);
-    free(beta_lnb);
-    free(x_out);
-    free(y_out);
-    free(z_out);
-    free(K);
-    free(E);
-    free(W);
+  cudaStreamDestroy(compute_stream);
+  cudaStreamDestroy(transfer_stream);
 
-    return 0;
+  cudaEventDestroy(start);
+  cudaEventDestroy(stop);
+
+  free(xi_lmk_cpu);
+  free(alpha_bl);
+  free(beta_lnb);
+  free(x_out);
+  free(y_out);
+  free(z_out);
+  free(K);
+  free(E);
+  free(W);
+
+  return 0;
 }
